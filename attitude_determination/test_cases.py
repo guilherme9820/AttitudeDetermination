1ff0ade52361d4d8e2840ed528819cbffbc4b7e3
Guilherme Henrique dos Santos <dos_santos_98@hotmail.com>
2021-06-27 12:27:54 -0300

    Removed unused files; Updated the TestCases class

--- attitude_determination/test_cases.py
+++ attitude_determination/test_cases.py
@@ -1,5 +1,4 @@
 import numpy as np
-import tensorflow as tf
 
 
 class TestCases:
@@ -10,41 +9,47 @@ class TestCases:
                                         [-0.864, 0.152, 0.480],
                                         [0.360, -0.480, 0.800]])
 
-    def __call__(self, num_samples=1, case='case1'):
-
-        case_fn = getattr(self, case)
-
-        return case_fn(num_samples)
-
     @property
     def true_attitude(self):
         return self._true_attitude
 
     @true_attitude.setter
     def true_attitude(self, values):
+        values = np.array(values)
+        assert values.shape == (3, 3), "The attitude matrix must be an array of shape (3, 3)"
+
         self._true_attitude = values
 
     def apply_noise(self, vectors, stddevs):
 
         batch_size = vectors.shape[0]
 
-        noise = tf.zeros([batch_size, 0, 3])
+        noise = np.zeros([batch_size, 0, 3])
         for std in stddevs:
-            temp = tf.random.normal([batch_size, 1, 3], 0, std)
-            noise = tf.concat([noise, temp], axis=1)
+            temp = np.random.normal(0, std, [batch_size, 1, 3])
+            noise = np.concatenate([noise, temp], axis=1)
 
         return vectors + noise
 
     def apply_rotation(self, vectors, attitudes):
 
-        vectors = tf.cast(vectors, tf.float32)
-        attitudes = tf.cast(attitudes, tf.float32)
+        vectors = np.asarray(vectors, dtype="float32")
+        attitudes = np.asarray(attitudes, dtype="float32")
 
-        rotated = tf.matmul(attitudes, vectors, transpose_b=True)
+        vectors = np.transpose(vectors, [0, 2, 1])
 
-        return tf.transpose(rotated, [0, 2, 1])
+        rotated = attitudes @ vectors
 
-    def build_data(self, ref_vectors, stddev):
+        return np.transpose(rotated, [0, 2, 1])
+
+    def build_data(self, ref_vectors, stddev, num_samples=1):
+
+        ref_vectors = np.asarray(ref_vectors)
+
+        if ref_vectors.ndim < 3:
+            ref_vectors = ref_vectors[np.newaxis, ...]
+
+        ref_vectors = np.tile(ref_vectors, [num_samples, 1, 1])
 
         body_vectors = self.apply_rotation(ref_vectors, self._true_attitude)
 
@@ -52,115 +57,71 @@ class TestCases:
 
         return ref_vectors, body_vectors, stddev
 
-    def case1(self, num_samples):
+    def __call__(self, num_samples=1, case='case1'):
 
-        ref_vectors = tf.convert_to_tensor([[[1., 0., 0.],
-                                             [0., 1., 0.],
-                                             [0., 0., 1.]]])
+        if case == "case1":
+            ref_vectors = [[1., 0., 0.],
+                           [0., 1., 0.],
+                           [0., 0., 1.]]
+            stddevs = [1e-6, 1e-6, 1e-6]
 
-        ref_vectors = tf.tile(ref_vectors, [num_samples, 1, 1])
+        elif case == "case2":
+            ref_vectors = [[1., 0., 0.],
+                           [0., 1., 0.]]
+            stddevs = [1e-6, 1e-6]
 
-        return self.build_data(ref_vectors, [1e-6, 1e-6, 1e-6])
+        elif case == "case3":
+            ref_vectors = [[1., 0., 0.],
+                           [0., 1., 0.],
+                           [0., 0., 1.]]
+            stddevs = [0.01, 0.01, 0.01]
 
-    def case2(self, num_samples):
+        elif case == "case4":
+            ref_vectors = [[1., 0., 0.],
+                           [0., 1., 0.]]
+            stddevs = [0.01, 0.01]
 
-        ref_vectors = tf.convert_to_tensor([[[1., 0., 0.],
-                                             [0., 1., 0.]]])
+        elif case == "case5":
+            ref_vectors = [[0.6, 0.8, 0.],
+                           [0.8, -0.6, 0.]]
+            stddevs = [1e-6, 0.01]
 
-        ref_vectors = tf.tile(ref_vectors, [num_samples, 1, 1])
+        elif case == "case6":
+            ref_vectors = [[1., 0., 0.],
+                           [1., 0.01, 0.],
+                           [1., 0., 0.01]]
+            stddevs = [1e-6, 1e-6, 1e-6]
 
-        return self.build_data(ref_vectors, [1e-6, 1e-6])
+        elif case == "case7":
+            ref_vectors = [[1., 0., 0.],
+                           [1., 0.01, 0.]]
+            stddevs = [1e-6, 1e-6]
 
-    def case3(self, num_samples):
+        elif case == "case8":
+            ref_vectors = [[1., 0., 0.],
+                           [1., 0.01, 0.],
+                           [1., 0., 0.01]]
+            stddevs = [0.01, 0.01, 0.01]
 
-        ref_vectors = tf.convert_to_tensor([[[1., 0., 0.],
-                                             [0., 1., 0.],
-                                             [0., 0., 1.]]])
+        elif case == "case9":
+            ref_vectors = [[1., 0., 0.],
+                           [1., 0.01, 0.]]
+            stddevs = [0.01, 0.01]
 
-        ref_vectors = tf.tile(ref_vectors, [num_samples, 1, 1])
+        elif case == "case10":
+            ref_vectors = [[1., 0., 0.],
+                           [.96, 0.28, 0.],
+                           [.96, 0., 0.28]]
+            stddevs = [1e-6, 0.01, 0.01]
 
-        return self.build_data(ref_vectors, [0.01, 0.01, 0.01])
+        elif case == "case11":
+            ref_vectors = [[1., 0., 0.],
+                           [.96, 0.28, 0.]]
+            stddevs = [1e-6, 0.01]
 
-    def case4(self, num_samples):
+        elif case == "case12":
+            ref_vectors = [[1., 0., 0.],
+                           [.96, 0.28, 0.]]
+            stddevs = [0.01, 1e-6]
 
-        ref_vectors = tf.convert_to_tensor([[[1., 0., 0.],
-                                             [0., 1., 0.]]])
-
-        ref_vectors = tf.tile(ref_vectors, [num_samples, 1, 1])
-
-        return self.build_data(ref_vectors, [0.01, 0.01])
-
-    def case5(self, num_samples):
-
-        ref_vectors = tf.convert_to_tensor([[[0.6, 0.8, 0.],
-                                             [0.8, -0.6, 0.]]])
-
-        ref_vectors = tf.tile(ref_vectors, [num_samples, 1, 1])
-
-        return self.build_data(ref_vectors, [1e-6, 0.01])
-
-    def case6(self, num_samples):
-
-        ref_vectors = tf.convert_to_tensor([[[1., 0., 0.],
-                                             [1., 0.01, 0.],
-                                             [1., 0., 0.01]]])
-
-        ref_vectors = tf.tile(ref_vectors, [num_samples, 1, 1])
-
-        return self.build_data(ref_vectors, [1e-6, 1e-6, 1e-6])
-
-    def case7(self, num_samples):
-
-        ref_vectors = tf.convert_to_tensor([[[1., 0., 0.],
-                                             [1., 0.01, 0.]]])
-
-        ref_vectors = tf.tile(ref_vectors, [num_samples, 1, 1])
-
-        return self.build_data(ref_vectors, [1e-6, 1e-6])
-
-    def case8(self, num_samples):
-
-        ref_vectors = tf.convert_to_tensor([[[1., 0., 0.],
-                                             [1., 0.01, 0.],
-                                             [1., 0., 0.01]]])
-
-        ref_vectors = tf.tile(ref_vectors, [num_samples, 1, 1])
-
-        return self.build_data(ref_vectors, [0.01, 0.01, 0.01])
-
-    def case9(self, num_samples):
-
-        ref_vectors = tf.convert_to_tensor([[[1., 0., 0.],
-                                             [1., 0.01, 0.]]])
-
-        ref_vectors = tf.tile(ref_vectors, [num_samples, 1, 1])
-
-        return self.build_data(ref_vectors, [0.01, 0.01])
-
-    def case10(self, num_samples):
-
-        ref_vectors = tf.convert_to_tensor([[[1., 0., 0.],
-                                             [.96, 0.28, 0.],
-                                             [.96, 0., 0.28]]])
-
-        ref_vectors = tf.tile(ref_vectors, [num_samples, 1, 1])
-
-        return self.build_data(ref_vectors, [1e-6, 0.01, 0.01])
-
-    def case11(self, num_samples):
-
-        ref_vectors = tf.convert_to_tensor([[[1., 0., 0.],
-                                             [.96, 0.28, 0.]]])
-
-        ref_vectors = tf.tile(ref_vectors, [num_samples, 1, 1])
-
-        return self.build_data(ref_vectors, [1e-6, 0.01])
-
-    def case12(self, num_samples):
-
-        ref_vectors = tf.convert_to_tensor([[[1., 0., 0.],
-                                             [.96, 0.28, 0.]]])
-
-        ref_vectors = tf.tile(ref_vectors, [num_samples, 1, 1])
-
-        return self.build_data(ref_vectors, [0.01, 1e-6])
+        return self.build_data(ref_vectors, stddevs, num_samples=num_samples)
